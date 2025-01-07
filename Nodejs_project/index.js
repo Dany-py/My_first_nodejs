c67b7d417ffd8a5a3ec10bae5a93868eaa935125
Dany-py <danydegbe@gmail.com>
2025-01-07 21:48:29 +0000

    Begining of the node.js project

---
+++ Nodejs_project/index.js
@@ -0,0 +1,12 @@
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello, Backend avec JavaScript sur Codespaces!');
+});
+
+app.listen(port, () => {
+    console.log(`Serveur en cours d'exécution sur http://localhost:${port}`);
+});
